096b053ca004986063c08e4be2677153876df254
Harry Prior <harryprior@hotmail.co.uk>
2022-09-17 17:13:29 +0100

    implement deposit

--- src/lib/client_mod/client.rs
+++ src/lib/client_mod/client.rs
@@ -2,37 +2,51 @@ use std::collections::{HashMap, HashSet};
 
 enum Tx {
     Withdraw(f64),
-    Deposit(f64)
+    Deposit(f64),
 }
 
-
-struct Client{
+struct Client {
     client: u16,
     available: f64,
     held: f64,
     total: f64,
     locked: bool,
     txs: HashMap<u32, Tx>,
-    disputed_txs: HashSet<u32>
+    disputed_txs: HashSet<u32>,
 }
 
-
 impl Client {
-    pub fn new(id: u16) -> Self{
-        Self{
+    pub fn new(id: u16) -> Self {
+        Self {
             client: id,
             total: 0.0,
             available: 0.0,
             held: 0.0,
             locked: false,
             txs: HashMap::new(),
-            disputed_txs: HashSet::new()
+            disputed_txs: HashSet::new(),
         }
     }
-    fn deposit(&mut self, tx: u32, amount: f64) -> (){todo!()}
-    fn withdraw(&mut self, tx: u32, amount: f64)-> (){todo!()}
-    fn dispute(&mut self, tx: u32)-> (){todo!()}
-    fn resolve(&mut self, tx: u32)-> (){todo!()}
-    fn chargeback(&mut self, tx: u32)-> (){todo!()}
-}
+    fn deposit(&mut self, tx: u32, amount: f64) -> () {
+        if self.locked {
+            return;
+        }
 
+        self.total += amount;
+        self.available += amount;
+        self.txs.insert(tx, Tx::Deposit(amount));
+    }
+
+    fn withdraw(&mut self, tx: u32, amount: f64) -> () {
+        todo!()
+    }
+    fn dispute(&mut self, tx: u32) -> () {
+        todo!()
+    }
+    fn resolve(&mut self, tx: u32) -> () {
+        todo!()
+    }
+    fn chargeback(&mut self, tx: u32) -> () {
+        todo!()
+    }
+}
