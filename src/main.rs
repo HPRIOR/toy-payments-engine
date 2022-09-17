f1bd74d1c07f60e0b97c994fdaadfea73b0c2923
Harry Prior <harryprior@hotmail.co.uk>
2022-09-17 17:46:47 +0100

    call lib from binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{env, process};
+
+use toy_payments_lib::process_payments;
+
+
 fn main() {
-    println!("Hello, world!");
-}
+    match env::args_os().nth(1) {
+        None => {
+            eprintln!("Missing csv file argumnet");
+            process::exit(1);
+        }
+        Some(csv_path) => {
+            match process_payments(&csv_path) {
+                Ok(result) => {
+                    println!("{}", result);
+                    process::exit(0);
+                }
+                Err(e) => {
+                    // error occured
+                    eprintln!("an error occured: {:#?}", e);
+                    process::exit(1);
+                }
+            }
+        }
+    }
+}  
