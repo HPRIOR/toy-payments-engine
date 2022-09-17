b3fd87a95051a404c54bef71e0cc00dc850e3d24
Harry Prior <harryprior@hotmail.co.uk>
2022-09-17 16:57:32 +0100

    add constructor

--- src/lib/client_mod/client.rs
+++ src/lib/client_mod/client.rs
@@ -11,11 +11,25 @@ struct Client{
     available: f64,
     held: f64,
     total: f64,
-    locked: f64,
+    locked: bool,
     txs: HashMap<u32, Tx>,
     disputed_txs: HashSet<u32>
 }
 
 
+impl Client {
+    pub fn new(id: u16) -> Self{
+        Self{
+            client: id,
+            total: 0.0,
+            available: 0.0,
+            held: 0.0,
+            locked: false,
+            txs: HashMap::new(),
+            disputed_txs: HashSet::new()
 
+
+        }
+    }
+}
 
